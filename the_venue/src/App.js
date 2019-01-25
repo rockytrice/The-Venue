e2c593ace71a1eee90614508706b4a84c2f4520c
Roderick Trice <rocky_trice@yahoo.com>
2019-01-25 01:52:15 -0500

    :lipstick: adding header and styling logo

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Header from "./components/header_footer/Header";
+import "./resources/styles.css";
 
 class App extends Component {
   render() {
-    return <div className="App">hello </div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
