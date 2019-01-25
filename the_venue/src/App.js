52da7f0ca10dd0fe0018fa53c1d99ebd52de29bb
Roderick Trice <rocky_trice@yahoo.com>
2019-01-25 13:12:14 -0500

    adding carrousel slider feature for cover images. used react slick for this component.

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Header from "./components/header_footer/Header";
+import Featured from "./components/featured/index";
 import "./resources/styles.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ height: "1500px", background: "blue" }}>
         <Header />
+        <Featured />
       </div>
     );
   }
