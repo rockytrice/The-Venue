6a788ba7e1b24cdec515f54dfb7ea645f165212c
Roderick Trice <rocky_trice@yahoo.com>
2019-01-27 00:42:59 -0500

    created reuseable button component

--- the_venue/src/components/Highlights/Discount.js
+++ the_venue/src/components/Highlights/Discount.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
+import MyButton from "../utils/Button";
 
 class Discount extends Component {
   state = {
@@ -44,7 +45,12 @@ class Discount extends Component {
                 esse iure.
               </p>
 
-              <div>button</div>
+              <MyButton
+                text="Purchase Tickets"
+                bck="#ffa800"
+                color="#ffffff"
+                link="http://google.com"
+              />
             </div>
           </Slide>
         </div>
