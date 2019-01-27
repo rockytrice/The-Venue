5a8b175ba1acdf28c99390bf8cf37ca6764e381c
Roderick Trice <rocky_trice@yahoo.com>
2019-01-26 23:04:02 -0500

    created a new Highlights folder and and Description component. Used another animation from react-reveal

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured/index";
 import VenueNfo from "./components/venueNfo/index";
+import Highlight from "./components/Highlights/index";
 import "./resources/styles.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: "blue" }}>
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
         <Featured />
         <VenueNfo />
+        <Highlight />
       </div>
     );
   }
